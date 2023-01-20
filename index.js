4f06a161060a9757a3b48e31f5816d445579a64e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-01-20 21:08:10 +0100

    ft: add in input validation

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const validBumpTypes = ['major', 'minor', 'revision'];
+
+try {
+  const bumpType = core.getInput('bump-type');
+
+  if (!validBumpTypes.includes(bumpType)) {
+    core.setFailed(`'${bumpType}' is not a valid bump type. Valid types are: ${validBumpTypes}.`);
+    return;
+  }
+
+  const rawTraverseDirs = core.getInput('traverse-dirs')
+  const traverseDirs = rawTraverseDirs?.toLowerCase() === 'true';
+
+} catch (e) {
+  core.setFailed(e);
+}
