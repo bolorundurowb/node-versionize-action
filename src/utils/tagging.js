259fc4416ee3094f4ea68dcca377420fbc890ccd
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-01-21 19:24:28 +0100

    ft: read and use the user email and password

--- src/utils/tagging.js
+++ src/utils/tagging.js
@@ -4,12 +4,17 @@ const simpleGit = require('simple-git');
 
 async function tagRelease(version) {
   const token = core.getInput('github-token');
+  const userName = core.getInput('user-name');
+  const userEmail = core.getInput('user-email');
   // const git = github.getOctokit(token);
   const context = github.context;
   // const userName =
-  core.debug(token);
-  core.debug(JSON.stringify(context));
+  core.info(token);
+  core.info(JSON.stringify(context));
   const git = simpleGit();
+  git.addConfig('user.name', userName);
+  git.addConfig('user.email', userEmail);
+  await git.add('.');
   await git.commit(`(chore): release v${version}`);
   await git.addTag(version);
   await git.push();
